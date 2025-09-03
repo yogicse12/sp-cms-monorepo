cfdd5320a5ba94892ea05046092e67853663d1de
Yogendra Yadav <yogendra@e9ine.com>
2025-09-03 16:52:49 +0530

    deploy fix in code

--- packages/sp-cms.api/src/index.ts
+++ packages/sp-cms.api/src/index.ts
@@ -3,7 +3,7 @@ import { Hono } from 'hono';
 const app = new Hono();
 
 app.get('/', c => {
-  return c.text('Hello from SP CMS API');
+  return c.text('This is the SP CMS API Project');
 });
 
 export default app;
