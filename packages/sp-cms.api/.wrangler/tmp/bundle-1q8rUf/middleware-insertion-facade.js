61b1198957499bb8315764d0cb7ed3c11aa13464
Yogendra Yadav <yogendra@e9ine.com>
2025-09-03 14:26:43 +0530

    project setup

---
+++ packages/sp-cms.api/.wrangler/tmp/bundle-1q8rUf/middleware-insertion-facade.js
@@ -0,0 +1,11 @@
+				import worker, * as OTHER_EXPORTS from "/Users/yogendrayadav/SideProjects/sp-cms-monorepo/packages/sp-cms.api/src/index.ts";
+				import * as __MIDDLEWARE_0__ from "/Users/yogendrayadav/SideProjects/sp-cms-monorepo/node_modules/.pnpm/wrangler@3.107.3_@cloudflare+workers-types@4.20250903.0/node_modules/wrangler/templates/middleware/middleware-ensure-req-body-drained.ts";
+import * as __MIDDLEWARE_1__ from "/Users/yogendrayadav/SideProjects/sp-cms-monorepo/node_modules/.pnpm/wrangler@3.107.3_@cloudflare+workers-types@4.20250903.0/node_modules/wrangler/templates/middleware/middleware-miniflare3-json-error.ts";
+
+				export * from "/Users/yogendrayadav/SideProjects/sp-cms-monorepo/packages/sp-cms.api/src/index.ts";
+
+				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
+					
+					__MIDDLEWARE_0__.default,__MIDDLEWARE_1__.default
+				]
+				export default worker;
